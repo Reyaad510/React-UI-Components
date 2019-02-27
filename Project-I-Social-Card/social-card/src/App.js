ddcc4cedfd7019ac3dbae2a028f4f131958bc3b0
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-27 13:45:31 -0500

    Created headertitle componenet

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
-
+import HeaderTitle from './components/HeaderComponents/HeaderTitle';
 const App = () => {
   return (
     <div>
     <ImageThumbnail />
+    <HeaderTitle />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+     <React.Fragment>
+    <h1>Lambda School</h1>
+    <p>@LambdaSchool </p>
+    <li>26 jan</li>
+    </React.Fragment>
+    );
+}
+
+export default HeaderTitle;
