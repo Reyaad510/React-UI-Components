16581fb3ecf974ae9688648ae8793196ce049340
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-27 13:35:51 -0500

    Completed imagethumbnail component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
+
 const App = () => {
   return (
     <div>
-    
+    <ImageThumbnail />
     </div>
   );
 };
