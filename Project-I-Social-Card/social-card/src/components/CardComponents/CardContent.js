c8de2da4e34323d58218b045a0a50324c2a6c274
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-27 20:14:11 -0500

    MVP completed, made header and card responsive

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -4,10 +4,11 @@ import './Card.css';
 const CardContent = () => {
     return (
         <div className="cardContent">
+        
         <h5> Get started with React</h5>
         <p> React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
         <p><a href="https://reactjs.org/" target="_blank">reactjs.org</a></p>
-        </div>
+         </div>
     );
 }
 
