c70846b22ff2c38a8a5534ecbb2d9fdcf3c280e6
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-27 21:34:36 -0500

    Made entire card clickable, footer completed

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,10 +5,10 @@ import './Footer.css';
 const Footer = () => {
     return (
         <nav className="icons">
-            <i class="far fa-comment fa-flip-horizontal"></i>
-            <i class="fas fa-sync"></i>
-            <i class="far fa-heart"></i>
-            <i class="far fa-envelope"></i>
+            <i class="far fa-comment fa-lg"></i>
+            <i class="fas fa-sync fa-lg">&nbsp;<span className="noBold">6</span></i>
+            <i class="far fa-heart fa-lg">&nbsp; 4</i>
+            <i class="far fa-envelope fa-lg"></i>
         </nav>
     );
 }
