eabc516e95cb7b2bc1226c55a8fabae8147f769e
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-28 23:46:27 -0500

    Boiler plates for action and number button.js with props

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+
 
 const App = () => {
   return (
     <div className='container'>
      <CalculatorDisplay />
+     <ActionButton text="clear" />
+     <NumberButton text="1" />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+
+const ActionButton = (props) => {
+    return (
+        <button>{props.text}</button>
+    );
+}
+
+export default ActionButton;
