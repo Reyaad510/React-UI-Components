4cfae57cac94b54b646015b7d9a5ca8aa4e53388
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-01 01:02:14 -0500

    MVP completed

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
@@ -10,22 +11,22 @@ const App = () => {
   return (
     <div className='container'>
      <CalculatorDisplay />
-     <ActionButton text="clear" />
-     <NumberButton text="&#247;" />
-     <NumberButton text="7" />
-     <NumberButton text="8" />
-     <NumberButton text="9" />
-     <NumberButton text="&#10005;" />
-     <NumberButton text="4" />
-     <NumberButton text="5" />
-     <NumberButton text="6" />
-     <NumberButton text="&#8722;" />
-     <NumberButton text="1" />
-     <NumberButton text="2" />
-     <NumberButton text="3" />
-     <NumberButton text="+" />
-     <ActionButton text="0" />
-     <NumberButton text="=" />
+        <ActionButton text="clear" />
+        <NumberButton buttonStyle='operator' text="&#247;" />
+        <NumberButton text="7" />
+        <NumberButton text="8" />
+        <NumberButton text="9" />
+        <NumberButton buttonStyle='operator' text="&#10005;" />
+        <NumberButton text="4" />
+        <NumberButton text="5" />
+        <NumberButton text="6" />
+        <NumberButton buttonStyle='operator' text="&#8722;" />
+        <NumberButton text="1" />
+        <NumberButton text="2" />
+        <NumberButton text="3" />
+        <NumberButton buttonStyle='operator' text="+" />
+        <ActionButton text="0" />
+        <NumberButton buttonStyle='operator' text="=" />
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Button.css';
 
 
+
+
+
 const NumberButton = (props) => {
     return (
         <button  className={`numberButton ${props.buttonStyle}`}>{props.text}</button>
